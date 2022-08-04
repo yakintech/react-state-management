d24688400f3d56de12f9911ebca1e095305c742a
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-08-04 21:30:00 +0300

    state sample

--- src/stateSample/ComplexSample.js
+++ src/stateSample/ComplexSample.js
@@ -2,19 +2,26 @@ import React from 'react'
 
 function ComplexSample() {
 
-    let test = () => {
+    const [nameValue, setnameValue] = useState('')
+    const [surnameValue, setsurnameValue] = useState('')
 
-        let testObject = {
-            name: nameState,
-            surname:surnameState
-        }
 
-        console.log(testObject);
 
+    let user = {
+        name: nameValue,
+        surname: surnameValue
     }
 
   return (
     <div>
+
+
+    <input type='text' value={nameValue} onChange={(e) => setnameValue(e.target.value)}/>
+
+
+    <input type='text' value={surnameValue} onChange={(e) => setsurnameValue(e.target.value)}/>
+
+
 
     </div>
   )
